58a8e10ec4d6c11e31fd2f7eaba771a5a25c599a
Никита <81732542+WildDeFox@users.noreply.github.com>
2024-11-22 18:20:22 +0700

    Шаблонные строки

--- Задачи/1. Основы/script.js
+++ Задачи/1. Основы/script.js
@@ -144,3 +144,15 @@
 // Решение 16.
 // let str = "Hello World";
 // alert(str.length);
+
+
+// Задача 17.
+// Перепишите следующий код через вставку переменных:
+/*
+let str1 = 'xxx';
+let str2 = 'yyy';
+let txt  = 'aaa ' + str1 + ' bbb ' + str2 + ' ccc'; 
+*/
+
+// Решение 17.
+// let txt = `aaa ${str1} bbb ${str2} ccc`;
