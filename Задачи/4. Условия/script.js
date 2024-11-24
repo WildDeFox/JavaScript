f311a148e73a976fe6ec2ccf65abda497b2557ee
defox <wildmrdefox@gmail.com>
2024-11-24 12:26:56 +0700

    Конструкция switch-case

--- Задачи/4. Условия/script.js
+++ Задачи/4. Условия/script.js
@@ -42,3 +42,34 @@
 //         console.log('Цифра двухзначна');
 //     }
 // }
+
+
+// Задача 4.
+// Перепишите следующий код через switch-case:
+// let lang = 'ru';
+// if (lang == 'ru') {
+// 	console.log('рус');
+// } else if (lang == 'en') {
+// 	console.log('анг');
+// } else if (lang == 'de') {
+// 	console.log('нем');
+// } else {
+// 	console.log('язык не поддерживается');
+// }
+
+// Решение 4.
+// let lang = 'ru';
+// switch (lang) {
+//     case 'ru':
+//         console.log('рус');
+//     break;
+//     case 'en':
+//         console.log('анг');
+//     break;
+//     case 'de':
+//         console.log('нем');
+//     break;
+//     default:
+//         console.log('Язык не поддерживается');
+//     break;
+// }
