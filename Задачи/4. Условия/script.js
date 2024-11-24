9f9cd3bb44edad1532a7829ab25330d949a09ff2
defox <wildmrdefox@gmail.com>
2024-11-24 11:58:04 +0700

    Сокращенная форма проверки на истину

---
+++ Задачи/4. Условия/script.js
@@ -0,0 +1,16 @@
+// Задача 1.
+// Перепишите следующий код с использованием сокращенной формы:
+// let test = true;
+// if (test === true) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+
+// Решение 1.
+// let test = true;
+// if (test) {
+//     console.log('+++');
+// } else {
+//     console.log('---');
+// }
