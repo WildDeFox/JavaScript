fe4b7037aa9c1896d38f878754f4b3d073dc33df
defox <wildmrdefox@gmail.com>
2024-11-30 12:41:35 +0700

    Степень и корень ч2.

--- Задачи/7. Стандартные методы/script.js
+++ Задачи/7. Стандартные методы/script.js
@@ -19,8 +19,9 @@
 // Для решения воспользуйтесь циклом for.
 
 // Решение
-let arr = [4, 2, 5, 19, 13, 0, 10];
-let sum = 0;
-for (let elem of arr) {
-    
-}
+// let arr = [4, 2, 5, 19, 13, 0, 10];
+// let sum = 0;
+// for (let elem of arr) {
+//     sum = Math.pow(elem, 3);
+// }
+// console.log(Math.sqrt(sum));
