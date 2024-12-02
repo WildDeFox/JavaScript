ac625c320f6328011abfcecb950c307ef2a5da36
defox <wildmrdefox@gmail.com>
2024-12-02 21:46:11 +0700

    Сумма элементов массива при рекурсии

--- Задачи/10. Рекурсия/script.js
+++ Задачи/10. Рекурсия/script.js
@@ -13,3 +13,19 @@
 // }
 // func(arr);
 
+
+// Задание 2.
+// Дан массив:
+// let arr = [1, 2, 3, 4, 5];
+// С помощью рекурсии найдите сумму квадратов элементов этого массива.
+// let arr = [1, 2, 3, 4, 5];
+// function getSum (arr) {
+//     let sum = arr.shift();
+
+//     if (arr.length != 0) {
+//         sum += getSum(arr);
+//     }
+
+//     return sum;
+// }
+// console.log(getSum(arr));
