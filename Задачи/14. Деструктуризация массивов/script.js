a22f90be394debe63a5f4eba10293eb8a1e08f68
Никита <81732542+WildDeFox@users.noreply.github.com>
2024-12-04 18:48:25 +0700

    Значения по умолчанию при деструктуризации объектов

--- Задачи/14. Деструктуризация массивов/script.js
+++ Задачи/14. Деструктуризация массивов/script.js
@@ -98,3 +98,27 @@
 // 	height: 500,
 // };
 // let {color: c, width: w, height: h} = options;
+
+
+// Задание 7.
+// В следующем коде части объекта записываются в соответствующие переменные:
+// let options = {
+// 	width:  400,
+// 	height: 500,
+// };
+// let color;
+// if (options.color !== undefined) {
+// 	color = options.color;
+// } else {
+// 	color = 'black';
+// }
+// let width  = options.width;
+// let height = options.height;
+// Переделайте этот код через деструктуризацию согласно изученной теории. 
+
+// Решение
+// let options = {
+// 	width:  400,
+// 	height: 500,
+// };
+// let {color = 'black', width, height} = options;
