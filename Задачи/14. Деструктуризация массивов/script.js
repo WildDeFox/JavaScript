891d22d3c6beb21e3d71d610220cec388cc728a4
Никита <81732542+WildDeFox@users.noreply.github.com>
2024-12-04 18:37:39 +0700

    Остаток массива при деструктуризации

--- Задачи/14. Деструктуризация массивов/script.js
+++ Задачи/14. Деструктуризация массивов/script.js
@@ -23,3 +23,17 @@
 // Решение
 // let arr = ['John', 'Smit', 'development', 'programmer', 2000]; 
 // let [, , department, programmer] = arr;
+
+
+// Задание 3.
+// В следующем коде части массива записываются в соответствующие переменные:
+// let arr = ['John', 'Smit', 'development', 'programmer', 2000]; 
+// let name    = arr[0];
+// let surname = arr[1];
+// let info = arr.slice(2);
+// Переделайте этот код через деструктуризацию согласно изученной теории. 
+
+// Решение
+// let arr = ['John', 'Smit', 'development', 'programmer', 2000]; 
+// let [name, surname, ...info] = arr;
+// console.log(info);
