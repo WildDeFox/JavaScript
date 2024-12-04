9484dd731ae761be3fe73ece71fd01456fed7f40
Никита <81732542+WildDeFox@users.noreply.github.com>
2024-12-04 18:53:34 +0700

    Переменные и значения по умолчанию

--- Задачи/14. Деструктуризация массивов/script.js
+++ Задачи/14. Деструктуризация массивов/script.js
@@ -122,3 +122,27 @@
 // 	height: 500,
 // };
 // let {color = 'black', width, height} = options;
+
+
+// Задание 8.
+//  В следующем коде части объекта записываются в соответствующие переменные:
+// let options = {
+// 	width:  400,
+// 	height: 500,
+// };
+// let с;
+// if (options.с !== undefined) {
+// 	с = options.color;
+// } else {
+// 	с = 'black';
+// }
+// let w = options.width;
+// let h = options.height;
+// Переделайте этот код через деструктуризацию согласно изученной теории. 
+
+// Решение 
+// let options = {
+// 	width:  400,
+// 	height: 500,
+// };
+// let {color: c = 'black', width: w, height: h} = options;
