1cd59d248a2866fc6fd79599034b8c8a693f2a70
Никита <81732542+WildDeFox@users.noreply.github.com>
2024-12-04 18:18:46 +0700

    Экстремальные значения массива через spread

--- Задачи/12. Оператор spread/script.js
+++ Задачи/12. Оператор spread/script.js
@@ -13,3 +13,11 @@
 // 	return num1 + num2 + num3 + num4 + num5;
 // }
 // console.log(func(...arr));
+
+
+// Задание 2.
+// Дан массив с числами. Используя Math.min и spread выведите на экран минимальное значение массива. 
+
+// Решение
+// let arr = [1, 2, 3, 4, 5, 6];
+// console.log(Math.min(...arr));
