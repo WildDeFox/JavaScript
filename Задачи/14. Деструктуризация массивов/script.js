9a3f28763cf2ec0c6a6fd0a7f2d0b0fe15eff657
Никита <81732542+WildDeFox@users.noreply.github.com>
2024-12-04 18:34:55 +0700

    Пропуск элементов массива при деструктуризации

--- Задачи/14. Деструктуризация массивов/script.js
+++ Задачи/14. Деструктуризация массивов/script.js
@@ -11,3 +11,15 @@
 // Решение
 // let arr = ['John', 'Smit', 'development', 'programmer', 2000]; 
 // let [name, surname, department, position, salary] = arr;
+
+
+// Задание 2.
+// В следующем коде части массива записываются в соответствующие переменные:
+// let arr = ['John', 'Smit', 'development', 'programmer', 2000]; 
+// let department = arr[2];
+// let position   = arr[3];
+// Переделайте этот код через деструктуризацию согласно изученной теории. 
+
+// Решение
+// let arr = ['John', 'Smit', 'development', 'programmer', 2000]; 
+// let [, , department, programmer] = arr;
