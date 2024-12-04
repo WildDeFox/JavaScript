d9f21d7f5c2071cca5122271fc7be6c713b91095
Никита <81732542+WildDeFox@users.noreply.github.com>
2024-12-04 18:16:19 +0700

    Оператор spread

---
+++ Задачи/12. Оператор spread/script.js
@@ -0,0 +1,15 @@
+// Задача 1.
+// Дан массив:
+// let arr = [1, 2, 3, 4, 5];
+// Дана также функция:
+// function func(num1, num2, num3, num4, num5) {
+// 	return num1 + num2 + num3 + num4 + num5;
+// }
+// Найдите с помощью приведенной функции сумму элементов массива. 
+
+// Решение
+// let arr = [1, 2, 3, 4, 5];
+// function func(num1, num2, num3, num4, num5) {
+// 	return num1 + num2 + num3 + num4 + num5;
+// }
+// console.log(func(...arr));
