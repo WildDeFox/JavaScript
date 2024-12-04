ca49303ba73869ecc10e0d46901f12e215c83583
Никита <81732542+WildDeFox@users.noreply.github.com>
2024-12-04 18:45:18 +0700

    Имена переменных при деструктуризации объектов

--- Задачи/14. Деструктуризация массивов/script.js
+++ Задачи/14. Деструктуризация массивов/script.js
@@ -77,3 +77,24 @@
 // 	height: 500,
 // };
 // let {color, width, height} = options;
+
+
+// Задание 6.
+// В следующем коде части объекта записываются в соответствующие переменные:
+// let options = {
+// 	color: 'red',
+// 	width:  400,
+// 	height: 500,
+// };
+// let c = options.color;
+// let w = options.width;
+// let h = options.height;
+// Переделайте этот код через деструктуризацию согласно изученной теории. 
+
+// Решение
+// let options = {
+// 	color: 'red',
+// 	width:  400,
+// 	height: 500,
+// };
+// let {color: c, width: w, height: h} = options;
