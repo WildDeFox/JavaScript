7732a13a4f45f56ad2bcd28065ed2a7d3ce6458c
Никита <81732542+WildDeFox@users.noreply.github.com>
2024-12-05 18:14:23 +0700

    Функция добавление нуля к числу

--- Функции/Генератор рандомных чисел/script.js
+++ Функции/Генератор рандомных чисел/script.js
@@ -7,3 +7,12 @@ function getRandomArbitary(min, max) {
 function getRandomInt(min, max) {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+// Добавляет 0 к числам от 1 до 9. Например 05. 
+function addZero(num) {
+	if (num >= 0 && num <= 9) {
+		return '0' + num;
+	} else {
+		return num;
+	}
+}
