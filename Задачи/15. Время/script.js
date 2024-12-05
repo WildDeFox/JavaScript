566abbcb73c03b81f6b68d1e231670c3c5598e53
Никита <81732542+WildDeFox@users.noreply.github.com>
2024-12-05 18:12:45 +0700

    Работа с объектом Date

---
+++ Задачи/15. Время/script.js
@@ -0,0 +1,22 @@
+// Задание 1.
+// Выведите на экран текущий день. 
+
+// Решение 
+// let date = new Date();
+// console.log(date.getDate());
+
+
+// Задание 2.
+// Выведите на экран текущий месяц. 
+
+// Решение 
+// let date = new Date();
+// console.log(date.getMonth());
+
+
+// Задание 3.
+// Выведите на экран текущий год. 
+
+// Решение 
+// let date = new Date();
+// console.log(date.getFullYear());
