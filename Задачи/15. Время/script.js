a8584b6348bbc9d6071d2d09e4f9a913f190030b
Никита <81732542+WildDeFox@users.noreply.github.com>
2024-12-05 18:20:21 +0700

    Форматирование даты

--- Задачи/15. Время/script.js
+++ Задачи/15. Время/script.js
@@ -20,3 +20,28 @@
 // Решение 
 // let date = new Date();
 // console.log(date.getFullYear());
+
+
+// Задание 4.
+// Выведите на экран текущую дату-время в формате 12:59:59 31.12.2014. 
+// Используйте для всех частей даты (кроме года) созданную нами функцию для добавления нуля при необходимости. 
+
+// Решение
+// function addZero(num) {
+// 	if (num >= 0 && num <= 9) {
+// 		return '0' + num;
+// 	} else {
+// 		return num;
+// 	}
+// }
+
+// let date = new Date();
+
+// console.log(
+//     addZero(date.getHours()) + ':' +
+//     addZero(date.getMinutes()) + ':' +
+//     addZero(date.getSeconds()) + ' ' +
+//     addZero(date.getDate()) + '.' +
+//     addZero(date.getMonth() + 1) + '.' +
+//     date.getFullYear() + '.'
+// );
