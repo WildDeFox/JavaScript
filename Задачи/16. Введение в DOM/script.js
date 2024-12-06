ea09420419da68fd2ebbee5cb811343f7ea5097d
Никита <81732542+WildDeFox@users.noreply.github.com>
2024-12-06 09:51:08 +0700

    Сложные селекторы DOM элемента

---
+++ Задачи/16. Введение в DOM/script.js
@@ -0,0 +1,27 @@
+// Задание 1.
+// Даны 3 абзаца:
+// <p id="elem1">1</p>
+// <p id="elem2">2</p>
+// <p id="elem3">3</p>
+// Запишите ссылку на каждый из абзацев в отдельную переменную и выведите содержимое каждой из этих переменных в консоль. 
+
+// Решение
+// let elem1 = document.querySelector('#elem1');
+// let elem2 = document.querySelector('#elem2');
+// let elem3 = document.querySelector('#elem3');
+// console.log(elem1);
+// console.log(elem2);
+// console.log(elem3);
+
+
+// Задание 2.
+// Дан следующий HTML:
+// <div id="block">
+// 	<p>1</p>
+// 	<p>2</p>
+// </div>
+// Получите ссылку на первый абзац из дива с id, равным block. 
+
+// Решение
+// let elem = document.querySelector('#block p');
+// console.log(elem);
