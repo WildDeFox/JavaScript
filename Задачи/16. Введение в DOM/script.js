69e92a52368991640b612bb33b1a00347f924500
Никита <81732542+WildDeFox@users.noreply.github.com>
2024-12-06 09:55:23 +0700

    Привязывание обработчиков к элементам

--- Задачи/16. Введение в DOM/script.js
+++ Задачи/16. Введение в DOM/script.js
@@ -25,3 +25,26 @@
 // Решение
 // let elem = document.querySelector('#block p');
 // console.log(elem);
+
+
+// Задание 3.
+// Даны 3 кнопки:
+// <input id="button1" type="submit">
+// <input id="button2" type="submit">
+// <input id="button3" type="submit">
+// Сделайте так, чтобы по клику на первую кнопку на экран выводилось число 1, 
+// по клику на вторую - число 2, а по клику на третью - число 3. 
+
+// Решение
+// let button1 = document.querySelector('#button1');
+// let button2 = document.querySelector('#button2');
+// let button3 = document.querySelector('#button3');
+// button1.addEventListener('click', function() {
+//     console.log('1');
+// })
+// button2.addEventListener('click', function() {
+//     console.log('2');
+// })
+// button3.addEventListener('click', function() {
+//     console.log('3');
+// })
