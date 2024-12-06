e682f6aa4bbac7df6e924a9d5f1b0435698cc590
Никита <81732542+WildDeFox@users.noreply.github.com>
2024-12-06 17:13:37 +0700

    Один обработчик ко многим элементам

--- Задачи/16. Введение в DOM/script.js
+++ Задачи/16. Введение в DOM/script.js
@@ -48,3 +48,32 @@
 // button3.addEventListener('click', function() {
 //     console.log('3');
 // })
+
+
+// Задание 4.
+// Даны 5 абзацев:
+// <p id="elem1">text</p>
+// <p id="elem2">text</p>
+// <p id="elem3">text</p>
+// <p id="elem4">text</p>
+// <p id="elem5">text</p>
+// Дана следующая функция:
+// function func() {
+// 	console.log('message');
+// }
+// Привяжите эту функцию ко всем 5-ти абзацам. 
+
+// Решение
+// let elem1 = document.querySelector('#elem1');
+// let elem2 = document.querySelector('#elem2');
+// let elem3 = document.querySelector('#elem3');
+// let elem4 = document.querySelector('#elem4');
+// let elem5 = document.querySelector('#elem5');
+// function func() {
+// 	console.log('message');
+// }
+// elem1.addEventListener('click', func);
+// elem2.addEventListener('click', func);
+// elem3.addEventListener('click', func);
+// elem4.addEventListener('click', func);
+// elem5.addEventListener('click', func);
