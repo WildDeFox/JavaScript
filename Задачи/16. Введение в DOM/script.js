cbb3fd2e3ce0024d72b4854abbba155ed816ff0f
defox <wildmrdefox@gmail.com>
2024-12-06 21:56:57 +0700

    Преимущество this

--- Задачи/16. Введение в DOM/script.js
+++ Задачи/16. Введение в DOM/script.js
@@ -336,3 +336,37 @@
 // button.addEventListener('click', function() {
 //     this.textContent = Number(this.textContent) + 1; 
 // });
+
+
+// Задание 26.
+// Даны 5 абзацев с какими-то текстами. 
+// По клику на любой абзац запишите в конец его текста восклицательный знак.
+
+// Решение
+// let elem1 = document.querySelector('#elem1');
+// let elem2 = document.querySelector('#elem2');
+// let elem3 = document.querySelector('#elem3');
+// let elem4 = document.querySelector('#elem4');
+// let elem5 = document.querySelector('#elem5');
+// function func() {
+//     this.textContent = this.textContent + '!';
+// }
+// elem1.addEventListener('click', func);
+// elem2.addEventListener('click', func);
+// elem3.addEventListener('click', func);
+// elem4.addEventListener('click', func);
+// elem5.addEventListener('click', func);
+
+
+// Задание 27.
+// Даны 3 инпута, в которых записаны какие-то числа. 
+// По потери фокуса в любом из инпутов возведите стоящее в нем число в квадрат.
+// let input1 = document.querySelector('#input1');
+// let input2 = document.querySelector('#input2');
+// let input3 = document.querySelector('#input3');
+// function func() {
+//     this.value = Number(this.value) * Number(this.value);
+// }
+// input1.addEventListener('blur', func);
+// input2.addEventListener('blur', func);
+// input3.addEventListener('blur', func);
