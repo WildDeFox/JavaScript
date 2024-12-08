f3293c0631bedd6c09934ed93d7f25f2be2a9081
defox <wildmrdefox@gmail.com>
2024-12-08 18:18:06 +0700

    Получение атрибутов

---
+++ Задачи/17. Атрибуты/script.js
@@ -0,0 +1,14 @@
+// Задание 1.
+// Получите значение его атрибута value.
+
+// Решение
+// let input = document.querySelector('#input');
+// console.log(input.getAttribute('value'));
+
+
+// Задание 2.
+// Получите значение его атрибута class.
+// let input = document.querySelector('#input');
+// console.log(input.getAttribute('class'));
+
+
