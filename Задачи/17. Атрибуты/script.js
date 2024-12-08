842f257765e0d380023d1e230aca448297e01a1e
defox <wildmrdefox@gmail.com>
2024-12-08 18:21:03 +0700

    Установка атрибутов

--- Задачи/17. Атрибуты/script.js
+++ Задачи/17. Атрибуты/script.js
@@ -12,3 +12,17 @@
 // console.log(input.getAttribute('class'));
 
 
+// Задание 3.
+// Установите его атрибут value в значение 'text'.
+
+// Решение
+// let input = document.querySelector('#input');
+// input.setAttribute('value', 'text');
+
+
+// Задание 4.
+// Установите ему атрибут class в значение 'valid'.
+
+// Решение
+// let input = document.querySelector('#input');
+// input.setAttribute('class', 'valid');
