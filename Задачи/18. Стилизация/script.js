deb6a6c3914cb60ec034b7265b2351a20dc33ffd
Никита <81732542+WildDeFox@users.noreply.github.com>
2024-12-09 18:43:41 +0700

    Стилизация элементов через атрибут style

---
+++ Задачи/18. Стилизация/script.js
@@ -0,0 +1,11 @@
+// Задача 1.
+// Дан див и кнопка. По клику на кнопку добавьте диву ширину, высоту и границу.
+
+// Решение
+// let div = document.querySelector('#div');
+// let button = document.querySelector('#button');
+// button.addEventListener('click', function() {
+//     div.style.width = '200px';
+//     div.style.height = '100px';
+//     div.style.border = '1px solid black';
+// })
