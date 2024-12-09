55cc93c951c82d13d7ef2a1ba5bac5fad4d25f73
defox <wildmrdefox@gmail.com>
2024-12-09 21:15:09 +0700

    Преимущество стилизации с помощью CSS классов

--- Задачи/18. Стилизация/script.js
+++ Задачи/18. Стилизация/script.js
@@ -44,3 +44,22 @@
 //     paragraph.classList = '';
 //     paragraph.classList.add('reda');
 // })
+
+
+// Задание 4.
+// Модифицируйте предыдущую задачу так, чтобы повторное нажатие на кнопку отменяло действие этой кнопки.
+
+// Решение
+// let paragraph = document.querySelector('#elem');
+// let button1 = document.querySelector('#button1');
+// let button2 = document.querySelector('#button2');
+// let button3 = document.querySelector('#button3');
+// button1.addEventListener('click', function() {
+//     paragraph.classList.toggle('cross');
+// });
+// button2.addEventListener('click', function() {
+//     paragraph.classList.toggle('fat');
+// });
+// button3.addEventListener('click', function() {
+//     paragraph.classList.toggle('reda')
+// });
