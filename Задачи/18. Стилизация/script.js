7fb542ebfa933b3eddf03d16af3ab45617282148
Никита <81732542+WildDeFox@users.noreply.github.com>
2024-12-09 18:47:04 +0700

    Стилизация свойств с дефисом

--- Задачи/18. Стилизация/script.js
+++ Задачи/18. Стилизация/script.js
@@ -9,3 +9,17 @@
 //     div.style.height = '100px';
 //     div.style.border = '1px solid black';
 // })
+
+
+// Задание 2.
+// Дан див с текстом и кнопка. 
+// По клику на кнопку установите диву размер шрифта в 20px, а также верхнюю границу и фон. 
+
+// Решение
+// let div = document.querySelector('#div');
+// let button = document.querySelector('#button');
+// button.addEventListener('click', function() {
+//     div.style.fontSize = '20px';
+//     div.style.borderTop = '1px solid black';
+//     div.style.backgroundColor = 'red';
+// })
