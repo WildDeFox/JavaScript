15e3020180e41b2254d2781d4c6ece9f47d6dfc4
defox <wildmrdefox@gmail.com>
2024-12-10 21:23:00 +0700

    Работа с текстареа

---
+++ Задачи/21. Формы/script.js
@@ -0,0 +1,9 @@
+// Задание 1.
+// Дан текстареа и абзац. По потери фокуса в текстареа запишите его текст в абзац.
+
+// Решение
+// let textarea = document.querySelector('#textarea');
+// let elem = document.querySelector('#elem');
+// textarea.addEventListener('blur', function() {
+//     elem.textContent = this.value;
+// })
