e133f15506ab962a90b9583d3d603ce4dd772691
Никита <81732542+WildDeFox@users.noreply.github.com>
2024-12-10 18:47:11 +0700

    Тип узлов

--- Задачи/20. Узлы/script.js
+++ Задачи/20. Узлы/script.js
@@ -19,3 +19,23 @@
 // for (let node of elem.childNodes) {
 //     console.log(node);
 // }
+
+
+// Задание 3.
+// Переберите циклом все узлы этого дива и выведите в консоль их названия. 
+
+// Решение
+// let elem = document.querySelector('#elem');
+// for (let node of elem.childNodes) {
+//     console.log(node.nodeName);
+// }
+
+
+// Задание 4.
+// Переберите циклом все узлы этого дива и выведите в консоль только узлы-элементы и текстовые узлы. 
+// let elem = document.querySelector('#elem');
+// for (let node of elem.childNodes) {
+//     if (node.nodeType == 1 || node.nodeType == 3) {
+//         console.log(node);
+//     }
+// }
