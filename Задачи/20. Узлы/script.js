cf4e31734640c4135bf702f54f5503e27d3523e9
Никита <81732542+WildDeFox@users.noreply.github.com>
2024-12-10 18:35:30 +0700

    Работа с узлами

---
+++ Задачи/20. Узлы/script.js
@@ -0,0 +1,11 @@
+// Задание 1.
+// Напишите код, который покажет разницу между lastChild и lastElementChild. 
+
+// Решение
+let elem = document.querySelector('#elem');
+console.log(elem.lastChild);
+console.log(elem.lastElementChild);
+console.log(elem.nextSibling)
+console.log(elem.nextElementSibling);
+console.log(elem.previousSibling);
+console.log(elem.previousElementSibling);
