6f3316f5606661472aae6141aa42a9dbd78f8114
defox <wildmrdefox@gmail.com>
2024-12-10 21:33:29 +0700

    Атрибут disabled

--- Задачи/21. Формы/script.js
+++ Задачи/21. Формы/script.js
@@ -7,3 +7,33 @@
 // textarea.addEventListener('blur', function() {
 //     elem.textContent = this.value;
 // })
+
+
+// Задание 2.
+// Дан инпут и кнопка. По нажатию на кнопку заблокируйте инпут.
+
+// Решение
+// let input = document.querySelector('#input');
+// let button = document.querySelector('#button');
+// button.addEventListener('click', function() {
+//     input.disabled = true;
+// })
+
+
+// Задание 3.
+// Дан инпут и две кнопки. 
+// Пусть нажатие на первую кнопку блокирует инпут, а нажатие на вторую - отблокирует.
+
+// Решение
+// let button1 = document.querySelector('#button1');
+// let button2 = document.querySelector('#button2');
+// let input = document.querySelector('#input');
+// button1.addEventListener('click', () => input.disabled = true)
+// button2.addEventListener('click', () => input.disabled = false)
+
+
+// Задание 4.
+// Дан инпут и кнопка. По нажатию на кнопку узнайте, заблокирован инпут или нет.
+// let input = document.querySelector('#input')
+// let button = document.querySelector('#button1');
+// button.addEventListener('click', () => console.log(input.disabled));
