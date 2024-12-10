2773c708919806691fb8653dce0a003c3c3bbaa6
Никита <81732542+WildDeFox@users.noreply.github.com>
2024-12-10 18:39:58 +0700

    Перебор узлов циклом

--- Задачи/20. Узлы/script.js
+++ Задачи/20. Узлы/script.js
@@ -2,10 +2,20 @@
 // Напишите код, который покажет разницу между lastChild и lastElementChild. 
 
 // Решение
-let elem = document.querySelector('#elem');
-console.log(elem.lastChild);
-console.log(elem.lastElementChild);
-console.log(elem.nextSibling)
-console.log(elem.nextElementSibling);
-console.log(elem.previousSibling);
-console.log(elem.previousElementSibling);
+// let elem = document.querySelector('#elem');
+// console.log(elem.lastChild);
+// console.log(elem.lastElementChild);
+// console.log(elem.nextSibling)
+// console.log(elem.nextElementSibling);
+// console.log(elem.previousSibling);
+// console.log(elem.previousElementSibling);
+
+
+// Задание 2.
+// Переберите циклом все узлы этого дива и выведите их в консоль. 
+
+// Решение
+// let elem = document.querySelector('#elem');
+// for (let node of elem.childNodes) {
+//     console.log(node);
+// }
