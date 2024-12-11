0c6b9b11ebb52cb93f3b0807d818a59896ca488e
Никита <81732542+WildDeFox@users.noreply.github.com>
2024-12-11 18:42:34 +0700

    Методы focus и blur

--- Задачи/21. Формы/script.js
+++ Задачи/21. Формы/script.js
@@ -159,3 +159,23 @@
 //         elem.textContent = `Размер текста привышает: ${input.value.length - 5} символов`;
 //     }
 // })
+
+
+// Задание 14.
+// Даны два инпута. 
+// Сделайте так, чтобы после ввода двух символов фокус ввода переходил ко второму инпуту, 
+// а после ввода двух символов в этот инпут - фокус из него убирался. 
+
+let input1 = document.querySelector('#input1');
+let input2 = document.querySelector('#input2');
+
+input1.addEventListener('input',() => {
+    if(input1.value.length > 1) {
+        input2.focus();
+    }
+});
+input2.addEventListener('input', () => {
+    if(input2.value.length > 1) {
+        input2.blur();
+    }
+})
