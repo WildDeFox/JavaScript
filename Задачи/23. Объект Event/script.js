e78e463f80f1ce73a9052fa8c86b7b209e955c26
Никита <81732542+WildDeFox@users.noreply.github.com>
2024-12-12 18:18:11 +0700

    Основы работы с объектом Event

---
+++ Задачи/23. Объект Event/script.js
@@ -0,0 +1,8 @@
+// Задание 1.
+// Самостоятельно проделайте описанные манипуляции и выведите объект с событием в консоль. 
+// Изучите структуру этого объекта. 
+
+// let button = document.querySelector('#button');
+// button.addEventListener('click', function(event) {
+//     console.log(event);
+// })
