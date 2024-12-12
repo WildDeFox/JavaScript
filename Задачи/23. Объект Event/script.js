7c036be9ecc5f097154497ed93034c2355fa6789
Никита <81732542+WildDeFox@users.noreply.github.com>
2024-12-12 18:22:05 +0700

    Координаты события

--- Задачи/23. Объект Event/script.js
+++ Задачи/23. Объект Event/script.js
@@ -6,3 +6,12 @@
 // button.addEventListener('click', function(event) {
 //     console.log(event);
 // })
+
+
+// Задание 2.
+// Сделайте так, чтобы при движении мышки по странице, отображались координаты курсора
+
+// let elem = document.querySelector('#elem');
+// document.addEventListener('mousemove', function(event){
+//     elem.innerHTML = event.clientX + ' : ' + event.clientY;
+// })
