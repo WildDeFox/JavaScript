79347283c69d9b9662b622c9e519861dc4282e1a
Никита <81732542+WildDeFox@users.noreply.github.com>
2024-12-12 18:25:41 +0700

    Тип события в объекте Event

--- Задачи/23. Объект Event/script.js
+++ Задачи/23. Объект Event/script.js
@@ -15,3 +15,22 @@
 // document.addEventListener('mousemove', function(event){
 //     elem.innerHTML = event.clientX + ' : ' + event.clientY;
 // })
+
+
+// Задание 3.
+// Допишите код функции func так, чтобы при клике на элемент этот элемент 
+// красился в зеленый цвет, а при двойном клике - в красный. 
+
+// let elem = document.querySelector('#elem');
+
+// elem.addEventListener('click', func);
+// elem.addEventListener('dblclick', func);
+
+// function func(event) {
+//     if (event.type == 'click') {
+//         elem.style.color = 'green';
+//     } else if (event.type == 'dblclick') {
+//         elem.style.color = 'red';
+//     }
+// }
+	
