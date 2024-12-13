ae62c2ce0d00ca352100cff4fe32c91717959b88
Никита <81732542+WildDeFox@users.noreply.github.com>
2024-12-13 18:10:52 +0700

    Контекст через переменную

---
+++ Задачи/24. Основы работы с контекстом/script.js
@@ -0,0 +1,13 @@
+// Задание 1.
+// let elem = document.querySelector('#elem');
+// elem.addEventListener('blur', parent);
+
+// function parent() {
+//     let self = this;
+
+//     function child() {
+//         console.log(self.value);
+//     }
+
+//     child();
+// }
