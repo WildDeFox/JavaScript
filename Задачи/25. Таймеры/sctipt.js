c1400fd4c05bb4aba3e72aa8c4a529421750b90b
defox <wildmrdefox@gmail.com>
2024-12-14 11:31:11 +0700

    Кнопки для запуска и остановки таймера

--- Задачи/25. Таймеры/sctipt.js
+++ Задачи/25. Таймеры/sctipt.js
@@ -41,11 +41,43 @@
 // Задание 5.
 // Возьмите ваше решение предыдущей задачи. 
 // Проверьте, что многократное нажатие на кнопку приводит к ускорению отсчета. Исправьте эту проблему.
-let button = document.querySelector('#start');
+// let button = document.querySelector('#start');
 
-button.addEventListener('click', function func() {
-    let count = 100;
+// button.addEventListener('click', function func() {
+//     let count = 100;
 
-    setInterval(() => console.log(count--), 1000)
-    this.removeEventListener('click', func);
-})
+//     setInterval(() => console.log(count--), 1000)
+//     this.removeEventListener('click', func);
+// })
+
+
+// Задание 6.
+// Пусть дана переменная, в которой изначально хранится число 100. 
+// Даны также две кнопки. По нажатию на первую кнопку запустите таймер, 
+// который каждую секунду будет уменьшать значение переменной на 1 и выводить новое значение в консоль. 
+// Как только значение переменной достигнет нуля - остановите таймер.
+
+// По нажатию на вторую кнопку остановите таймер. 
+// Также остановите таймер, если вторая кнопка не была нажата, но значение переменной достигло нуля.
+
+// let start = document.querySelector('#start');
+// let stop = document.querySelector('#stop');
+
+// let timerId;
+
+// start.addEventListener('click', function start() {
+//     let count = 100;
+
+//     timerId = setInterval(() => {
+//         console.log(count--);
+//         if (count == 0) {
+//             clearInterval(timerId);
+//         }
+//     }, 1000);
+
+//     this.removeEventListener('click', start);
+// });
+
+// stop.addEventListener('click', function() {
+//     clearInterval(timerId);
+// })
