0173e4f724921036fb8cb15377ca2f4175ed9ee9
defox <wildmrdefox@gmail.com>
2024-12-14 17:16:33 +0700

    Создание элементов в цикле

--- Задачи/25. Манипулирование элементами/script.js
+++ Задачи/25. Манипулирование элементами/script.js
@@ -24,14 +24,27 @@
 // Дан ol и кнопка. Сделайте так, чтобы по клику на кнопку в конец ol добавлялся тег li. 
 // Сделайте так, чтобы по клику на любой из добавленных li в конец его текста записывался восклицательный знак.
 
-let ol = document.querySelector('#ol');
-let button = document.querySelector('#button');
+// let ol = document.querySelector('#ol');
+// let button = document.querySelector('#button');
 
-button.addEventListener('click', function() {
-    let elem = document.createElement('li');
-    elem.textContent = 'item';
-    elem.addEventListener('click', function() {
-        this.textContent = this.textContent + '!';
-    });
-    ol.appendChild(elem);
-})
+// button.addEventListener('click', function() {
+//     let elem = document.createElement('li');
+//     elem.textContent = 'item';
+//     elem.addEventListener('click', function() {
+//         this.textContent = this.textContent + '!';
+//     });
+//     ol.appendChild(elem);
+// })
+
+
+// Задание 4.
+// Дан пустой ul. Запустите цикл, который вставит в него 10 тегов li. 
+// Сделайте так, чтобы текстом каждой li был ее порядковый номер.
+
+// let ul = document.querySelector('#ul');
+
+// for(let i = 1; i <= 10; i++) {
+//     let elem = document.createElement('li');
+//     elem.textContent = i;
+//     ul.appendChild(elem);
+// }
