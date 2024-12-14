7a4a529abf9dcd383e55e96c57a4b7012a3b4cf1
defox <wildmrdefox@gmail.com>
2024-12-14 17:08:59 +0700

    Создание и вставка элементов

---
+++ Задачи/25. Манипулирование элементами/script.js
@@ -0,0 +1,21 @@
+// Задание 1.
+// Вставьте ему в конец li с текстом 'item'.
+
+// let ol = document.querySelector('#elem');
+// let elem = document.createElement('li');
+// elem.textContent = 'item';
+// ol.appendChild(elem);
+
+
+// Задание 2.
+// Сделайте так, чтобы по клику на кнопку в конец списка добавлялся li с текстом 'item'.
+
+// let button = document.querySelector('#button');
+// let ol = document.querySelector('#elem');
+
+// button.addEventListener('click', function() {
+//     let elem = document.createElement('li');
+//     elem.textContent = 'item';
+//     ol.appendChild(elem);
+// });
+
