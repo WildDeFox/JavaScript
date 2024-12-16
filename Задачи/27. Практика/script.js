56f3b020a69b03256767cc9529675075ab2673a0
defox <wildmrdefox@gmail.com>
2024-12-16 12:01:57 +0700

    Создание элементов из массива

---
+++ Задачи/27. Практика/script.js
@@ -0,0 +1,15 @@
+// Задание 1.
+// Создание элементов из массива
+// let arr = [1, 2, 3, 4, 5];
+// let parent = document.querySelector('#parent');
+
+// for (let elem of arr) {
+//     let li = document.createElement('li');
+//     li.textContent = elem;
+
+//     li.addEventListener('click', function() {
+//         this.textContent += '1';
+//     });
+
+//     parent.appendChild(li);
+// }
