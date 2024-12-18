ae18d576f7e4db7a67e6a3d98e28605322f7012b
WildMrDeFox <wildmrdefox01@gmail.com>
2024-12-18 17:22:04 +0700

    Калькулятор площади прямоугольника

--- Проекты/Калькуляторы фигур/js/script.js
+++ Проекты/Калькуляторы фигур/js/script.js
@@ -12,6 +12,12 @@ let triangleSide = document.querySelector('#triangleSide');
 let result3 = document.querySelector('#result3');
 let button3 = document.querySelector('#button3');
 
+// Переменные для площади прямоугольника
+let rectangleSideA = document.querySelector('#rectangleSideA');
+let rectangleSideB = document.querySelector('#rectangleSideB');
+let result4 = document.querySelector('#result4');
+let button4 = document.querySelector('#button4');
+
 // Нахождение площади квадрата
 squareArea.addEventListener('blur', function(){
     result1.textContent = Math.pow(Number(squareArea.value), 2);
@@ -22,6 +28,12 @@ squarePerimeter.addEventListener('blur', function() {
     result2.textContent = Number(squarePerimeter.value) * 4;
 });
 
+// Нахождение площади треугольника
 button3.addEventListener('click', function() {
     result3.textContent = (Number(triangleBase.value) * Number(triangleSide.value) / 2);
+})
+
+// Нахождение площади прямоугольника
+button4.addEventListener('click', function() {
+    result4.textContent = Number(rectangleSideA.value) * Number(rectangleSideB.value);
 })
