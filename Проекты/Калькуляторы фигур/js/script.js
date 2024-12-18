44a24a2afd7ed17271e92843d017f0df497b3256
WildMrDeFox <wildmrdefox01@gmail.com>
2024-12-18 15:29:52 +0700

    Калькулятор периметра квадрата

--- Проекты/Калькуляторы фигур/js/script.js
+++ Проекты/Калькуляторы фигур/js/script.js
@@ -2,7 +2,16 @@
 let squareArea = document.querySelector('#squareArea');
 let result1 = document.querySelector('#result1');
 
+// Переменные для периметра квадрата
+let squarePerimeter = document.querySelector('#squarePerimeter');
+let result2 = document.querySelector('#result2');
+
 // Нахождение площади квадрата
 squareArea.addEventListener('blur', function(){
     result1.textContent = Math.pow(Number(squareArea.value), 2);
+});
+
+// Нахождение периметра квадрата
+squarePerimeter.addEventListener('blur', function() {
+    result2.textContent = Number(squarePerimeter.value) * 4;
 });
