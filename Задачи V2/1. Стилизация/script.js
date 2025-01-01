a2cf1685694dd6ec617f07a1278409a58f79aaea
defox <wildmrdefox@gmail.com>
2025-01-01 13:59:11 +0700

    Единицы измерения в атрибуте style

---
+++ Задачи V2/1. Стилизация/script.js
@@ -0,0 +1,20 @@
+// Задание 1.
+// Дан див и кнопка. По нажатию на кнопку задайте диву ширину в 400px, а высоту в 300px.
+// let button = document.querySelector('#button');
+// let div = document.querySelector('#div');
+
+// button.addEventListener('click', function() {
+//     div.style.width = "400px";
+//     div.style.height = "300px";
+// })
+
+
+// Задание 2.
+// Дана также кнопка. По клику на кнопку выведите ширину и высоту дива без единиц измерения.
+// let elem = document.querySelector('#elem');
+// let button = document.querySelector('#button');
+
+// button.addEventListener('click', function() {
+//     console.log(parseInt(elem.style.width));
+//     console.log(parseInt(elem.style.height));
+// })
