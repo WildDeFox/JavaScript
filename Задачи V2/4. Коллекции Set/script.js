7caa897e68f4d0950dd8409e8b68dda146397fb3
defox <wildmrdefox@gmail.com>
2025-01-06 08:18:22 +0700

    Получение DOM элементов без дублей

--- Задачи V2/4. Коллекции Set/script.js
+++ Задачи V2/4. Коллекции Set/script.js
@@ -60,3 +60,27 @@
 // let arr = [1, 2, 3, 3, 3, 4, 5, 5];
 // let newArr = clearArr(arr);
 // console.log(newArr);
+
+
+// Задание 8.
+// Даны абзацы и кнопка. 
+// Пользователь кликает на эти абзацы в произвольном порядке. 
+// Сделайте так, чтобы по нажатию на кнопку в конец каждого абзаца, 
+// на который был совершен клик, был добавлен восклицательный знак.
+
+// let elems = document.querySelectorAll('p');
+// let button = document.querySelector('#button');
+
+// let set = new Set;
+
+// for (let elem of elems) {
+//     elem.addEventListener('click', function() {
+//         set.add(this);
+//     });
+// }
+
+// button.addEventListener('click', function() {
+//     for (let elem of set) {
+//         elem.textContent = elem.textContent + "!";
+//     }
+// })
