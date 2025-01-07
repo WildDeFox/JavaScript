b162c7ed2f4bec3a9cd1d3f7ff0da5c6df297574
defox <wildmrdefox@gmail.com>
2025-01-07 08:21:32 +0700

    Операторы повторения символов в регулярках

--- Задачи V2/5. Регулярные выражения/sctipt.js
+++ Задачи V2/5. Регулярные выражения/sctipt.js
@@ -14,3 +14,24 @@
 // Напишите регулярку, которая найдет строки 'abba' и 'abea', не захватив 'adca'.
 // let str = 'aba aca aea abba adca abea';
 // console.log(str.replace(/ab.a/g, '!'));
+
+
+// Задание 4.
+// Напишите регулярку, которая найдет строки 'aba', 'abba', 'abbba' по шаблону: 
+// буква 'a', буква 'b' любое количество раз, буква 'a'.
+// let str = 'aa aba abba abbba abca abea';
+// console.log(str.replace(/ab+a/g, "!"));
+
+
+// Задание 5.
+// Напишите регулярку, которая найдет строки 'aa', 'aba', 'abba', 'abbba' по шаблону: 
+// буква 'a', буква 'b' любое количество раз (в том числе ниодного раза), буква 'a'.
+// let str = 'aa aba abba abbba abca abea';
+// console.log(str.replace(/ab*a/g, "!"));
+
+
+// Задание 6.
+// Напишите регулярку, которая найдет строки 'aa', 'aba' по шаблону: 
+// буква 'a', буква 'b' один раз или ниодного, буква 'a'.
+// let str = 'aa aba abba abbba abca abea';
+// console.log(str.replace(/ab?a/g, "!"));
